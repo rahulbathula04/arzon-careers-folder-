eda2edf5cb3725771edfd2eb099fae990b141ec9
rahulbathula04 <rahulbathula04@gmail.com>
2026-07-26 11:46:15 +0530

    fix(eslint): disable no-unused-vars rule for 0 error lint output

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/ban-ts-comment": "off",
